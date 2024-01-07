df74ebc24d213dfea45076f04f648f723afd3b99
Isha Varrier <ishavarrier@gmail.com>
2024-01-06 16:07:42 -0800

    Update blogPreview.tsx to include _doc format

--- src/app/components/blogPreview.tsx
+++ src/app/components/blogPreview.tsx
@@ -5,32 +5,28 @@ import { IBlog } from "@/database/blogSchema";
 import Link from "next/link";
 import { format } from "date-fns";
 
-export default function BlogPreview(props: IBlog) {
+export default function BlogPreview(props: { _doc: IBlog }) {
+  const { title, date, description, slugURL, shortdescription, image } = props._doc; 
+  //^^this is extracting the specific properties from props._doc which is just a way of retriving
+  //the data from mongoose. 
+
   return (
     <div className={style.centeredcontainer}>
-      <div>
-        <div>
-          <main>
-            <div className="blog_container"> </div>
-          </main>
-
           <div className="footer">
             <footer>
               © 2023 Personal Website Isha Varrier | All Rights Reserved
             </footer>
-          </div>
-        </div>
+          </div>    
 
         <div className="row">
           <div className="card">
-            <Link href={`/blogs/${props.slugURL}`}>
-              <h2>{props.title}</h2>
+            <Link href={`/blogs/${slugURL}`}>
+              <h2>{title}</h2>
             </Link>
-            <h5>Published on, {format(new Date(props.date), 'LLLL d, yyyy')}</h5>
-            <p>{props.shortdescription}</p>
+            <h5>Published on, {format(new Date(date), 'LLLL d, yyyy')}</h5>
+            <p>{shortdescription}</p>
           </div>
         </div>
-      </div>
     </div>
   );
 }
